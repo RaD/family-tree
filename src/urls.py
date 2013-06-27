b445850068adb1b16b8801fd0da9447880625cc1
Ruslan Popov <ruslan.popov@gmail.com>
2013-06-28 03:42:13 +0400

    Support static in developer environment.

--- src/urls.py
+++ src/urls.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 
+from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 admin.autodiscover()
 
@@ -10,3 +12,11 @@ urlpatterns = patterns(
     url(r'^admin/', include(admin.site.urls)),
     url(r'^dropzone/', include('src.dropzone.urls', namespace='dropzone')),
 )
+
+urlpatterns += staticfiles_urlpatterns()
+
+if settings.DEBUG:
+    urlpatterns += patterns(
+        '',
+        url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.MEDIA_ROOT, }), )
